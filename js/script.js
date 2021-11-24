29c29736c1f88d383b929c62b27c5df52e405b40
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-24 19:48:08 +0100

    almost

--- js/script.js
+++ js/script.js
@@ -8,3 +8,69 @@
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
+
+
+CreateClasses();
+
+
+
+
+function CreateClasses () {
+  
+// Creo le classi attraverso un ciclo
+for(let i= 0; i < 5; i++) {
+// Seleziono i box in cui inserire le classi
+  const ContainerBox = document.getElementById('container');
+  const Internbox = document.createElement("div");
+  Internbox.classList.add("intern-box");
+  ContainerBox.appendChild(Internbox);
+
+  const ClassNumber = document.createElement("div");
+  ClassNumber.classList.add("number");
+   Internbox.appendChild(ClassNumber);
+
+  
+//  Inserisco i numeri
+let Random = Math.floor(Math.random()*99) + 1;
+ClassNumber.innerHTML = (Random);
+
+
+function TimeUtente () {
+  ClassNumber.innerHTML = "";
+
+};
+
+
+
+}
+
+setTimeout(TimeUtente,3000) ;
+
+};
+
+
+
+
+
+const UtenteNumeri = parseInt(prompt("Inserisci  cinque numeri che hai visto precedentemente"));
+
+
+
+
+// Richiesta all'utente dei numeri
+
+
+// // verifica(UtenteNumeri);
+
+
+// // Verificare il prompt
+// function verifica(member) {
+
+// if (isNaN(member))
+// {
+// alert ("Non hai inserito un numero");
+
+// return false;
+
+// }} ;
